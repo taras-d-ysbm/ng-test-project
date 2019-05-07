561314945f0bf74fde2b07f9666fcad025934041
Taras Darchuk <taras.darchuk@gmail.com>
2019-05-07 15:25:52 +0200

    styles, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './modules/home/home.module#HomeModule'
+  },
+  {
+    path: 'orders',
+    loadChildren: './modules/orders/orders.module#OrdersModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
